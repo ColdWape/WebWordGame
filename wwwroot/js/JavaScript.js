31818da5e47a557d2454db6f3949a877c525f572
ColdWape <92642047+ColdWape@users.noreply.github.com>
2021-10-21 05:00:10 +0300

    Added content on start page

--- wwwroot/js/JavaScript.js
+++ wwwroot/js/JavaScript.js
@@ -1,1 +1,35 @@
-﻿
+﻿var userName = "q";
+
+function showAuthTypeChoosing() {
+    if (userName == "") {
+        document.getElementById("startBtns").style.display = 'none';
+        document.getElementById("gameTypeChoosing").style.display = 'none';
+        document.getElementById("gameQuantityPeopleChoosing").style.display = 'none';
+        document.getElementById("authTypeChoosingBtns").style.display = 'inline';
+
+    }
+    else showGameTypeChoosingMenu();
+
+}
+
+
+
+function showGameTypeChoosingMenu() {
+    document.getElementById("startBtns").style.display = 'none';
+    document.getElementById("gameTypeChoosing").style.display = 'inline';
+    document.getElementById("gameQuantityPeopleChoosing").style.display = 'none';
+
+}
+
+function showMainMenu() {
+    document.getElementById("startBtns").style.display = 'inline';
+    document.getElementById("gameTypeChoosing").style.display = 'none';
+    document.getElementById("gameQuantityPeopleChoosing").style.display = 'none';
+}
+
+function showGameQuantityPeopleChoosing() {
+    document.getElementById("gameQuantityPeopleChoosing").style.display = 'inline';
+    document.getElementById("gameTypeChoosing").style.display = 'none';
+    document.getElementById("startBtns").style.display = 'none';
+
+}
